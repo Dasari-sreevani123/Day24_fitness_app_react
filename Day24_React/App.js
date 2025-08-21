f2a7fb03b4379acac36786ff0ae14975c1959e14
Sreevani Dasari <sreevanidasari55@gmail.com>
2025-08-21 08:48:25 +0530

    Add files via upload

---
+++ Day24_React/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FitnessDashboard from "./components/FitnessDashboard";
+
+function App() {
+  return (
+    <div>
+      <FitnessDashboard />
+    </div>
+  );
+}
+
+export default App;
